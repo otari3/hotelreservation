a62ef3b42213dcfe8e86cc8555f93b8ed2b83dde
otari nozadze <otikonozadze@gmail.com>
2024-11-10 19:21:34 +0400

    creates users model

--- hotelres/users/models.py
+++ hotelres/users/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 from shared import models as base_model,erros
+from hotels.models import Hotels  
 
 # Create your models here.
+
+class User(models.Model):
+  name = models.TextField()
+  personal_id = models.IntegerField()
+  hotel = models.ForeignKey(Hotels,on_delete=models.CASCADE)
+  class Meta:
+    unique_together = ('hotel', 'personal_id')
